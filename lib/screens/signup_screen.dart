a5e35522d81702d677c0e8fd28a51ac015c8876e
Aniket2812 <aniketyadav982@gmail.com>
2025-10-12 23:33:22 +0530

    signup screen complete

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,5 +1,6 @@
 import 'package:clickpic/constants/colors.dart';
 import 'package:clickpic/widgets/country_code_dropdown.dart';
+import 'package:clickpic/widgets/custom_button.dart';
 import 'package:clickpic/widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
 
@@ -13,6 +14,8 @@ class SignupScreen extends StatefulWidget {
 class _SignupScreenState extends State<SignupScreen> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _numController = TextEditingController();
+  final TextEditingController _pwdController = TextEditingController();
+  final TextEditingController _confirmPwdController = TextEditingController();
   String _selectedCountryCode = '+91';
 
   @override
@@ -34,7 +37,7 @@ class _SignupScreenState extends State<SignupScreen> {
     return Image.asset(
       'assets/images/login_header.png',
       width: double.infinity,
-      height: screenHeight * 0.31,
+      height: screenHeight * 0.30,
       fit: BoxFit.cover,
     );
   }
@@ -43,7 +46,7 @@ class _SignupScreenState extends State<SignupScreen> {
     return SingleChildScrollView(
       child: Column(
         children: [
-          SizedBox(height: screenHeight * 0.26),
+          SizedBox(height: screenHeight * 0.25),
           Container(
             width: double.infinity,
             constraints: BoxConstraints(minHeight: screenHeight * 0.73),
@@ -58,8 +61,14 @@ class _SignupScreenState extends State<SignupScreen> {
                 _buildWelcomeHeader(),
                 const SizedBox(height: 20),
                 _buildNameSection(),
-                const SizedBox(height: 15,),
-                _buildPhoneNumberSection()
+                const SizedBox(height: 15),
+                _buildPhoneNumberSection(),
+                const SizedBox(height: 15),
+                _buildCreatePasswordSection(),
+                const SizedBox(height: 15),
+                _buildConfirmPasswordSection(),
+                const SizedBox(height: 20),
+                _buildBottomSection(),
               ],
             ),
           ),
@@ -72,15 +81,24 @@ class _SignupScreenState extends State<SignupScreen> {
     return Column(
       children: [
         Center(
-          child: Text(
-            'Welcome Onboard!',
-            style: TextStyle(
-              fontWeight: FontWeight.w700,
-              fontSize: 24,
-              color: AppColors.primary,
+          child: ShaderMask(
+            shaderCallback: (bounds) => const LinearGradient(
+              colors: [Color(0xFF006C80), Color(0xFF00414D)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ).createShader(bounds),
+            child: const Text(
+              'Welcome Onboard!',
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 24,
+                color: Colors
+                    .white, 
+              ),
             ),
           ),
         ),
+
         Center(
           child: Text.rich(
             TextSpan(
@@ -144,6 +162,64 @@ class _SignupScreenState extends State<SignupScreen> {
               ),
             ),
           ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildCreatePasswordSection() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _buildLabel('Create Password'),
+        const SizedBox(height: 5),
+        CustomTextField(
+          controller: _pwdController,
+          hintText: 'Enter Password',
+          width: double.infinity,
+          keyboardType: TextInputType.text,
+          isObscureText: true,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildConfirmPasswordSection() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _buildLabel('Confirm Password'),
+        const SizedBox(height: 5),
+        CustomTextField(
+          controller: _confirmPwdController,
+          hintText: 'Enter Password Again',
+          width: double.infinity,
+          keyboardType: TextInputType.text,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildBottomSection() {
+    return Column(
+      children: [
+        CustomButton(onTap: () {}, text: 'Sign Up'),
+        const SizedBox(height: 15),
+        Text.rich(
+          TextSpan(
+            text: 'Already a user?? ',
+            style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+            children: const <TextSpan>[
+              TextSpan(
+                text: 'Log In ',
+                style: TextStyle(
+                  color: AppColors.primary,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              TextSpan(text: 'to Continue'),
+            ],
+          ),
         ),
       ],
     );
